767a5b76f8eceba3bd2db669ccda3677074ecb70
israr.mahmood <israr.mahmood1@gmail.com>
2017-11-12 15:48:23 +0000

    backing up

--- server.py
+++ server.py
@@ -18,6 +18,8 @@ from mod_scraper import get_mod_class
 from pySOT_dict import pySOT_class_dict
 from controller_obj import controller_obj
 
+from time import sleep
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
@@ -38,35 +40,6 @@ def checkering(msg):
 def make_correction_to_data (parsed_json):
     if parsed_json['adaptive_sampling']['weights'] == -1:
         parsed_json['adaptive_sampling']['weights'] = None
-
-def get_arguments_for_stratagy(parsed_json):
-    print(parsed_json)
-
-    new_dict_obj = pySOT_class_dict()
-    class_dict = new_dict_obj.get_dict()
-    if class_dict == -1:
-        emit('error_msg', 'Could not find dependencies')
-        return
-
-    new_obj = pySOT_obj(parsed_json, class_dict)
-    [sucess, msg] = new_obj.run()
-    if not sucess:
-        print(msg+'\n\n\n')
-        emit('error_msg', msg)
-        return
-
-    [data, exp_des, surrogate, adapt_samp, maxeval] = new_obj.return_values()
-
-    print(data.info)
-
-    return { 'worker_id' : 0 , 
-             'data' : data , 
-             'maxeval' : maxeval , 
-             'nsamples' : parsed_json["controller"]["threads"] , 
-             'exp_design' : exp_des , 
-             'response_surface' : surrogate , 
-             'sampling_method' : adapt_samp }
-
 
 @socketio.on('run')
 def onMsgRun(msg):
@@ -101,12 +74,6 @@ def onMsgRun(msg):
 
     print(pySOTObj['data'].info)
 
-    # This point onwards
-
-    # if parsed_json["controller"]["function"] == "SerialController":
-    #     controller = SerialController(pySOTObj['data'].objfunction)
-    # else:
-    #     controller = ThreadController()
     print('controller start')
     new_controller = controller_obj(parsed_json, pySOTObj, [checkering,], class_dict)
     print('controller init')
@@ -117,66 +84,6 @@ def onMsgRun(msg):
         emit('error_msg', controller)
         return
 
-    #nsamples = parsed_json["strategy"]["nsamples"]
-
-#     # (5) Use the sychronous strategy without non-bound constraints
-# strategy = SyncStrategyNoConstraints(
-#         worker_id=0, data=data, maxeval=maxeval, nsamples=1,
-#         exp_design=exp_des, response_surface=surrogate,
-#         sampling_method=adapt_samp)
-# controller.strategy = strategy
-
-# # Run the optimization strategy
-# result = controller.run()
-
-# # Print the final result
-# print('Best value found: {0}'.format(result.value))
-# print('Best solution found: {0}'.format(
-#     np.array_str(result.params[0], max_line_width=np.inf,
-#                 precision=5, suppress_small=True)))
-
-
-
-
-# controller = ThreadController()
-
-# # (5) Use the sychronous strategy without non-bound constraints
-# # Use 4 threads and allow for 4 simultaneous evaluations
-# nthreads = 4
-# strategy = SyncStrategyNoConstraints(
-#         worker_id=0, data=data, maxeval=maxeval, nsamples=nthreads,
-#         exp_design=exp_des, response_surface=surrogate,
-#         sampling_method=adapt_samp)
-# controller.strategy = strategy
-
-# # Launch the threads and give them access to the objective function
-# for _ in range(nthreads):
-#     worker = BasicWorkerThread(controller, data.objfunction)
-#     controller.launch_worker(worker)
-
-# # Run the optimization strategy
-# result = controller.run()
-
-
-
-
-
-
-
-
-
-
-    # controller = SerialController(data.objfunction)
-    # print('got to this point')
-    # # (5) Use the sychronous strategy without non-bound constraints
-    # strategy = SyncStrategyNoConstraints(
-    #         worker_id=0, data=pySOTObj['data'], maxeval=pySOTObj['maxeval'], nsamples=nsamples,
-    #         exp_design=pySOTObj['exp_design'], response_surface=pySOTObj['response_surface'],
-    #         sampling_method=pySOTObj['sampling_method'])
-    # controller.strategy = strategy
-    # controller.feval_callbacks = [checkering,]
-
-    # Run the optimization strategy
     result = controller.run()
 
     # Print the final result
@@ -206,72 +113,23 @@ def onMsgRun(msg):
     print(' ')
     print(json.dumps(class_dict))
 
-    #hiding_this_in_a_function(data, exp_des, surrogate, adapt_samp, parsed_json['surrogate_model']['maxp'])
-
     return
-
-
-
-def objective(x):
-    time.sleep(1)
-    return (x-0.123)*(x-0.123)
-
-def init_controller():
-    samples = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
-    controller = ThreadController()
-    strategy = FixedSampleStrategy(samples)
-    strategy = CheckWorkerStrategy(controller, strategy)
-    controller.strategy = strategy
-
-    for _ in range(5):
-        worker = BasicWorkerThread(controller, objective)
-        controller.launch_worker(worker)
-    return controller
-
-@socketio.on('c_connect')
-def onConnect(msg):
-    print('Message: ' + msg)
-    send(msg)
-
-def onMessage(payload, isBinary):
-    if isBinary:
-        return
-        pass
-
-    handlers = {
-        'run': self.onMsgRun,
-        'terminate': self.onMsgTerminate
-    }
-    print("Text message received: {0}".format(payload.decode('utf8')))
-    msg = json.loads(payload.decode('utf8'))
-    handlers[msg['type'].decode('utf8')](msg)
-
-manager = None
-
-@socketio.on('run_')
-def onMsgRun(msg):
-    print(msg)
-    manager = Manager(init_controller())
-    #reactor.callInThread(self.manager.run, self)
-    manager.run()
 
 @socketio.on('terminate_')
 def onMsgTerminate(msg):
     print(msg)
     reactor.callInThread(self.manager.terminate)
 
-
-
-
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
-    send(msg, broadcast=True)
-
-@socketio.on('this_event')
-def handleMessage(msg):
-    print('Message1000: ' + msg)
-
+    #send(msg , broadcast=True)
+    emit('abc',msg)
+    i=0
+    while i<10:
+        print('the happeing ' + str(i))
+        i+=1
+        sleep(1)
 
 if __name__ == '__main__':
     import sys
