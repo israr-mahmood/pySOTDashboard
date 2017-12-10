1dccaef862463ec0f4718c0a3d8319f1ee139310
israr-mahmood <israr.mahmood1@gmail.com>
2017-12-10 14:40:10 -0500

    Create __init__.py
    Adding package to project

---
+++ pySOTDashboard/__init__.py
@@ -0,0 +1,9 @@
+from controller_object import *
+from delay_function import *
+from module_scraper import *
+from projection_function import *
+from pySOT_dictionary import *
+from pySOT_object import *
+
+__version__ = '0.1.0'
+__author__ = 'Israr Mahmood, Peiyu Shi'
